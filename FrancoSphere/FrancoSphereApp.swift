7570f11583a6d9630778578e4ad9fdcf777c0a5f
Shawn Magloire <Smf813@gmail.com>
2025-03-01 14:54:21 -0500

    Initial Commit

---
+++ FrancoSphere/FrancoSphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrancoSphereApp.swift
+//  FrancoSphere
+//
+//  Created by Shawn Magloire on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct FrancoSphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FrancoSphereTests/FrancoSphereTests.swift
@@ -0,0 +1,17 @@
+//
+//  FrancoSphereTests.swift
+//  FrancoSphereTests
+//
+//  Created by Shawn Magloire on 3/1/25.
+//
+
+import Testing
+@testable import FrancoSphere
+
+struct FrancoSphereTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
